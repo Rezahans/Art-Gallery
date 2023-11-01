0cae53109050800220d5cd977a26e48fce09b864
Rezahans <rezacrent.1@gmail.com>
2023-11-01 22:25:35 +0700

    Challang BEJS Chapter 6

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express'),
     app = express(),
     port = process.env.PORT || 7000,
-    router = require('./routes/router'),
+    controller = require('./controllers/artControllers'),
+    router = express.Router(),
+    multer = require('multer')();
     cors = require('cors');
 
 app.use(express.json({ strict : false}))
 app.use(cors())
-app.use('/api/v1', router)
+app.post('/create-with-imagekit', multer.single('image') , controller.createWithImageKit);
+app.get('/art', controller.getAllArt);
+app.get('/art/:artId', controller.getArtById);
+app.put('/art/:artId', controller.update);
+app.delete('/art/:artId', controller.delete);
+
 
 app.listen(port, () => {
     console.log(`Server is running at PORT ${port}`)
