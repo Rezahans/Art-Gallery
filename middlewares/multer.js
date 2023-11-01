7c77fc29ef03688112d7cee20384d82f84f2ad32
Rezahans <rezacrent.1@gmail.com>
2023-11-01 21:14:33 +0700

    Challang BEJS Chapter 6

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -2,19 +2,18 @@ const multer = require('multer');
 const path = require('path');
 
 const filename = (req, file, callback) => {
-    console.log(file)
     const fileName = Date.now() + '-' + file.originalname;
-    callback(null, fileName)
+    callback(null, fileName);
 };
 
 const generateStore = (destination) => {
     return multer.diskStorage({
         destination: (req, file, callback) => {
-            callback(null, destination)
+            callback(null, destination);
         },
-        filename
-    })
-}
+        filename,
+    });
+};
 
 module.exports = {
     image: multer({
@@ -22,14 +21,14 @@ module.exports = {
         fileFilter: (req, file, callback) => {
             const allowed = ['image/png', 'image/jpg', 'image/jpeg'];
 
-            if(allowed.includes(file.mimetype)) {
-                callback(null, true)
+            if (allowed.includes(file.mimetype)) {
+                callback(null, true);
             } else {
-                const err = new Error(`Only ${allowed.join(', ')} are allowed to upload`)
+                callback(new Error(`Only ${allowed.join(', ')} are allowed to upload`), false);
             }
         },
         onError: (err, next) => {
-            next(err)
-        }
-    })
-}
+            next(err);
+        },
+    }),
+};
