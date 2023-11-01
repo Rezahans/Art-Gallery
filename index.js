eaf6e27c0c3b7839225668acf631c3f1f1ed0c3b
Rezahans <rezacrent.1@gmail.com>
2023-11-01 22:20:38 +0700

    Challang BEJS Chapter 6

--- index.js
+++ index.js
@@ -4,18 +4,9 @@ const express = require('express'),
     router = require('./routes/router'),
     cors = require('cors');
 
-    require("dotenv").config();
 app.use(express.json({ strict : false}))
 app.use(cors())
-// app.use('/images', express.static('public/images'))
 app.use('/api/v1', router)
-
-// Handle 404 route
-app.get('*', (req, res) => {
-    return res.status(404).json({
-        error: 'End point is not registered'
-    })
-})
 
 app.listen(port, () => {
     console.log(`Server is running at PORT ${port}`)
